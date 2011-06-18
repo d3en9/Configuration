591f29f36348711673bca599ffd2ec75acc04372
Eugene Mahalin <efmahalin@gmail.com>
2011-06-18 10:51:10 +0400

    Add test project

--- Configuration/Configuration.cs
+++ Configuration/Configuration.cs
@@ -3,9 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Configuration
+namespace AppConfig
 {
+    #region Types
+    public enum ParameterState
+    {
+        Unchanged = 0,
+        Changed,
+        Added,
+        Deleted,
+        Unknown
+    }
+
+    public class AttributeInfo
+    {
+        public string Value { get; set; }
+        public string DefaultValue { get; set; }
+        public ParameterState State { get; set; }
+
+        private AttributeInfo(){ }
+
+        public AttributeInfo(string DefaultValue)
+        {
+            this.Value = null;
+            this.DefaultValue = DefaultValue;
+            this.State = ParameterState.Unknown;
+        }
+
+    }
+    #endregion
+
     public class Configuration
     {
+
     }
 }
